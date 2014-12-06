d01af3c750a05035a5c73d18b418b051c6880b5d
Juan Pablo Kutianski <jkutianski@gmail.com>
2014-12-06 06:06:01 -0300

    missing semicolo9n

--- scripts/change_version.js
+++ scripts/change_version.js
@@ -1,4 +1,4 @@
-var packageJSON = require(process.cwd() + '/package.json')
+var packageJSON = require(process.cwd() + '/package.json'),
 	fs = require('fs'),
 	version = packageJSON.version.split('.'),
 	build = version[2];
@@ -9,6 +9,8 @@ packageJSON.version = version.slice(0,2).join('.');
 packageJSON.version += ('.' + build);
 
 fs.writeFile(process.cwd() + '/package.json', JSON.stringify(packageJSON, null, 4), function (err) {
-  if (err) throw err;
+  if (err) {
+  	throw err;
+  }
   console.log("%s new version %s",packageJSON.name.toUpperCase(), packageJSON.version);
 });
