fa37f0aa1d2628c358a3df739f6be4524f8648e5
Juan Pablo Kutianski <jkutianski@gmail.com>
2014-12-06 01:10:46 -0300

    Add test from endpoints.json

---
+++ test/testEndpoints.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+var key = require('./key.json'),
+	meetup = require('../lib/meetup')(key),
+	endpoints = require('../lib/endpoints.json'),
+	assert = require('assert');
+
+console.log("%s ver %s Test\n\n", meetup.description, meetup.version);
+
+function objectType(obj) {
+	switch (typeof obj) {
+		case 'object':
+			return (Array.isArray(obj)) ? "array" : "object";
+			break;
+		default:
+			return "unknown"
+	}
+
+}
+
+Object.keys(endpoints).filter(function(endpointkey) {
+	return endpoints[endpointkey].hasOwnProperty('test');
+}).forEach(function(endpointkey) {
+	meetup[endpointkey](endpoints[endpointkey].test.params, function(err, ret) {
+
+		assert(!ret.problem, ret.code + '\n' + ret.details + '\n');
+		assert(!ret.errors, JSON.stringify(ret.errors));
+
+		assert.equal(objectType(ret), endpoints[endpointkey].test.return.type, endpointkey + ' not return an ' + endpoints[endpointkey].test.return.type);
+
+		switch (objectType(ret)) {
+			case 'object':
+				assert(endpoints[endpointkey].test.return.keys, endpointkey + ' hasn\'t defined keys on endpoints.json');
+
+				endpoints[endpointkey].test.return.keys.forEach(function(returnkey) {
+					assert(ret.hasOwnProperty(returnkey), endpointkey + ' not return the ' + returnkey + ' key');
+				});
+
+				break;
+			default:
+		}
+
+		console.log('%s\t\tPASS', endpointkey)
+	});
+});
+
+process.on('exit', function(code) {
+	if (!code) {
+		console.log("\nDone!")
+	}
+});
