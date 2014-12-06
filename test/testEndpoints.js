d9a1a3f6435fac733066b82ed47f8ccc0380a16a
Juan Pablo Kutianski <jkutianski@gmail.com>
2014-12-06 15:50:15 -0300

    cleanup Indentation

--- test/testEndpoints.js
+++ test/testEndpoints.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
+
 var assert = require('assert');
 
 assert(process.env.MEETUP_KEY, 'MEETUP_KEY variable isn\'t set on enviroment (use \'set "MEETUP_KEY=key"\' on Windows)');
 
-var meetup = require('../lib/meetup')({key: process.env.MEETUP_KEY}),
+var meetup = require('../lib/meetup')({
+		key: process.env.MEETUP_KEY
+	}),
 	endpoints = require('../lib/endpoints.json');
 
 console.log("%s ver %s Test\n\n", meetup.description, meetup.version);
@@ -19,31 +22,33 @@ function objectType(obj) {
 
 }
 
-Object.keys(endpoints).filter(function(endpointkey) {
-	return endpoints[endpointkey].hasOwnProperty('test');
-}).forEach(function(endpointkey) {
-	meetup[endpointkey](endpoints[endpointkey].test.params, function(err, ret) {
+Object.keys(endpoints)
+	.filter(function(endpointkey) {
+		return endpoints[endpointkey].hasOwnProperty('test');
+	})
+	.forEach(function(endpointkey) {
+		meetup[endpointkey](endpoints[endpointkey].test.params, function(err, ret) {
 
-		assert(!ret.problem, ret.code + '\n' + ret.details + '\n');
-		assert(!ret.errors, JSON.stringify(ret.errors));
+			assert(!ret.problem, ret.code + '\n' + ret.details + '\n');
+			assert(!ret.errors, JSON.stringify(ret.errors));
 
-		assert.equal(objectType(ret), endpoints[endpointkey].test.return.type, endpointkey + ' not return an ' + endpoints[endpointkey].test.return.type);
+			assert.equal(objectType(ret), endpoints[endpointkey].test.return.type, endpointkey + ' not return an ' + endpoints[endpointkey].test.return.type);
 
-		switch (objectType(ret)) {
-			case 'object':
-				assert(endpoints[endpointkey].test.return.keys, endpointkey + ' hasn\'t defined keys on endpoints.json');
+			switch (objectType(ret)) {
+				case 'object':
+					assert(endpoints[endpointkey].test.return.keys, endpointkey + ' hasn\'t defined keys on endpoints.json');
 
-				endpoints[endpointkey].test.return.keys.forEach(function(returnkey) {
-					assert(ret.hasOwnProperty(returnkey), endpointkey + ' not return the ' + returnkey + ' key');
-				});
+					endpoints[endpointkey].test.return.keys.forEach(function(returnkey) {
+						assert(ret.hasOwnProperty(returnkey), endpointkey + ' not return the ' + returnkey + ' key');
+					});
 
-				break;
-			default:
-		}
+					break;
+				default:
+			}
 
-		console.log('%s\t\tPASS', endpointkey);
+			console.log('%s\t\tPASS', endpointkey);
+		});
 	});
-});
 
 process.on('exit', function(code) {
 	if (!code) {
