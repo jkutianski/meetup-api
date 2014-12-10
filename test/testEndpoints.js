7fe3b1824f66ff1b69a64d06f2e1396ab5e648da
Juan Pablo Kutianski <jkutianski@gmail.com>
2014-12-10 06:53:47 -0300

    prepare to test streaming

--- test/testEndpoints.js
+++ test/testEndpoints.js
@@ -4,10 +4,12 @@ var assert = require('assert');
 
 assert(process.env.MEETUP_KEY, 'MEETUP_KEY variable isn\'t set on enviroment (use \'set "MEETUP_KEY=key"\' on Windows)');
 
+var endpoints = require('../lib/endpoints.json'),
+	commands = Object.keys(endpoints);
+
 var meetup = require('../lib/meetup')({
-		key: process.env.MEETUP_KEY
-	}),
-	endpoints = require('../lib/endpoints.json');
+	key: process.env.MEETUP_KEY
+}, commands);
 
 console.log("%s ver %s Test\n\n", meetup.description, meetup.version);
 
@@ -17,50 +19,76 @@ function objectType(obj) {
 			return (Array.isArray(obj)) ? "array" : "object";
 			break;
 		default:
-			return "unknown";
+			return typeof obj;
 	}
+};
 
-}
+var checkEndpoint = {};
 
-Object.keys(endpoints)
+checkEndpoint.http = function (endpointkey) {
+	meetup[endpointkey](endpoints[endpointkey].test.params, function(err, ret) {
+		(function(errors) {
+			ret.problem = errors || ret.problem;
+			ret.code = errors && errors.code || ret.problem;
+			ret.details = errors && errors.message || ret.details;
+
+		})(
+			ret && ret.errors && ret.errors.pop()
+		);
+
+		assert(!ret.problem, ret.code + ' (' + ret.details + ')\n');
+
+		assert(!err, err);
+
+		assert.equal(objectType(ret), endpoints[endpointkey].test.return.type, endpointkey + ' not return an ' + endpoints[endpointkey].test.return.type);
+
+		switch (objectType(ret)) {
+			case 'object':
+				assert(endpoints[endpointkey].test.return.keys, endpointkey + ' hasn\'t defined keys on endpoints.json');
+
+				endpoints[endpointkey].test.return.keys.forEach(function(returnkey) {
+					assert(ret.hasOwnProperty(returnkey), endpointkey + ' not return the ' + returnkey + ' key');
+				});
+
+				break;
+			default:
+		}
+
+		console.log('%s\t\tPASS', endpointkey);
+	});
+};
+
+checkEndpoint.ws = function (endpointkey) {
+	var ws = meetup[endpointkey](endpoints[endpointkey].params)
+		.on('data', function(ret) {
+			ws.abort();
+			switch (objectType(ret)) {
+				case 'object':
+					assert(endpoints[endpointkey].test.return.keys, endpointkey + ' hasn\'t defined keys on endpoints.json');
+
+					endpoints[endpointkey].test.return.keys.forEach(function(returnkey) {
+						assert(ret.hasOwnProperty(returnkey), endpointkey + ' not return the ' + returnkey + ' key');
+					});
+
+					break;
+				default:
+			}
+		})
+		.on('close', function () {
+			console.log('%s\t\tPASS', endpointkey);
+		});
+};
+
+commands
 	.filter(function(endpointkey) {
-		return endpoints[endpointkey].hasOwnProperty('test');
+		return endpoints[endpointkey].hasOwnProperty('test') &&
+			!endpoints[endpointkey].test.hasOwnProperty('disabled') &&
+			!endpoints[endpointkey].resource.match(/^ws\:/);
 	})
 	.forEach(function(endpointkey, index) {
-		setTimeout(
-			function() {
-				meetup[endpointkey](endpoints[endpointkey].test.params, function(err, ret) {
-
-					(function(errors) {
-						ret.problem = errors || ret.problem;
-						ret.code = errors && errors.code || ret.problem;
-						ret.details = errors && errors.message || ret.details;
-
-					})(
-						ret && ret.errors && ret.errors.pop()
-					);
-
-					assert(!ret.problem, ret.code + ' (' + ret.details + ')\n');
-
-					assert.equal(objectType(ret), endpoints[endpointkey].test.return.type, endpointkey + ' not return an ' + endpoints[endpointkey].test.return.type);
-
-					switch (objectType(ret)) {
-						case 'object':
-							assert(endpoints[endpointkey].test.return.keys, endpointkey + ' hasn\'t defined keys on endpoints.json');
-
-							endpoints[endpointkey].test.return.keys.forEach(function(returnkey) {
-								assert(ret.hasOwnProperty(returnkey), endpointkey + ' not return the ' + returnkey + ' key');
-							});
-
-							break;
-						default:
-					}
-
-					console.log('%s\t\tPASS', endpointkey);
-				});
-			},
-			1000 * index + 1
-		);
+		setTimeout(function() {
+			checkEndpoint.http(endpointkey);
+		}, 1000 * index + 1);
 	});
 
 process.on('exit', function(code) {
