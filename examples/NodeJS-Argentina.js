ff87700877d0eaf987b6f8d80c8d4ecfc79d1487
Juan Pablo Kutianski <jkutianski@gmail.com>
2014-12-13 17:46:17 -0300

    return errors

--- examples/NodeJS-Argentina.js
+++ examples/NodeJS-Argentina.js
@@ -1,14 +1,17 @@
-var assert = require('assert'),
-	key = process.env.MEETUP_KEY;
+var assert = require('assert');
 
-assert(key, 'MEETUP_KEY variable isn\'t set on enviroment (use \'set "MEETUP_KEY=key"\' on Windows)');
+assert(process.env.MEETUP_KEY, 'MEETUP_KEY variable isn\'t set on enviroment (use \'set "MEETUP_KEY=key"\' on Windows)');
 
 var meetup = require('../lib/meetup')({
-	key: key
+	key: process.env.MEETUP_KEY
 });
 
 meetup.getGroup({
 	"urlname": "NodeJS-Argentina"
 }, function(err, obj) {
-	console.log("%s <%s> (%s members)", obj.name, obj.link, obj.members);
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("%s <%s> (%s members)", obj.name, obj.link, obj.members);
+	}
 });
