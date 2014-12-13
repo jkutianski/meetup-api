a0386c8db04f02540ea8cdb3456e281d8ac2c47e
Juan Pablo Kutianski <jkutianski@gmail.com>
2014-12-13 19:04:46 -0300

    added example using 'self' as 'member_id'

---
+++ examples/myevents.js
@@ -0,0 +1,17 @@
+﻿var assert = require('assert');
+
+assert(process.env.MEETUP_KEY, 'MEETUP_KEY variable isn\'t set on enviroment (use \'set "MEETUP_KEY=key"\' on Windows)');
+
+var meetup = require('../lib/meetup')({
+	key: process.env.MEETUP_KEY
+});
+
+meetup.getEvents({
+	'member_id': 'self'
+}, function(error, events) {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log(events);
+	}
+});
