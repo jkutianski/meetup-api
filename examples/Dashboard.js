ab2ef8d33bb9be67c3ac98ad61c5b5c48f9f336a
Juan Pablo Kutianski <jkutianski@gmail.com>
2014-12-20 08:04:57 -0300

    delete unused parameter

--- examples/Dashboard.js
+++ examples/Dashboard.js
@@ -4,9 +4,9 @@ var assert = require('assert'),
 assert(key, 'MEETUP_KEY variable isn\'t set on enviroment (use \'set "MEETUP_KEY=key"\' on Windows)');
 
 var meetup = require('../lib/meetup')({
-		key: key
-	},['dashboard']);
+	key: key
+});
 
-meetup.dashboard({}, function(err,obj) {
+meetup.dashboard({}, function(err, obj) {
 	console.log("nearby events: %s, upcoming events: %s", obj.stats.nearby_events, obj.stats.upcoming_events);
 });
