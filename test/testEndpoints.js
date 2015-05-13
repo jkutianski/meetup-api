56369af8e2cfd51d9e9ccf87e95491880ecfc5e0
Juan Pablo Kutianski <jkutianski@gmail.com>
2015-05-13 02:25:43 -0300

    Added support for chunked HTTP

--- test/testEndpoints.js
+++ test/testEndpoints.js
@@ -1,25 +1,27 @@
-#!/usr/bin/env node
+/*jslint node: true */
+'use strict';
 
-console.log("NOTE: Don't abuse with the test, or your credentials will be throttled!!!");
+console.log('NOTE: Don\'t abuse with the test, or your credentials will be throttled!!!');
 
 var assert = require('assert'),
 	forAllAsync = require('forallasync').forAllAsync;
 
-assert(process.env.MEETUP_KEY, 'MEETUP_KEY variable isn\'t set on enviroment (use \'set "MEETUP_KEY=key"\' on Windows)');
+assert(process.env.MEETUP_KEY, 'MEETUP_KEY variable isn\'t set on enviroment (use \'set \"MEETUP_KEY=key\"\' on Windows)');
 
 var meetup = require('../lib/meetup')();
 
 var endpoints = require('../lib/endpoints.json');
 
-console.log("%s ver %s Test\n\n", meetup.description, meetup.version);
+console.log('%s ver %s Test\n\n', meetup.description, meetup.version);
 
 assert(!meetup.authkey, 'Authkey getter failed');
 console.log('Authkey getter\t\tPASS');
 meetup.authkey = process.env.MEETUP_KEY;
 assert(meetup.authkey, 'Authkey setter failed');
 console.log('Authkey setter\t\tPASS');
-
-meetup.dependencies.superagent = 0;
+try {
+	meetup.dependencies.superagent = 0;
+} catch (err) {}
 assert(meetup.dependencies.superagent !== 0, 'Read Only properties can be writed');
 console.log('Read Only properties\t\tPASS');
 
@@ -28,7 +30,7 @@ console.log('\nModule functions check:\n');
 function objectType(obj) {
 	switch (typeof obj) {
 		case 'object':
-			return (Array.isArray(obj)) ? "array" : "object";
+			return (Array.isArray(obj)) ? 'array' : 'object';
 		default:
 			return typeof obj;
 	}
@@ -37,37 +39,61 @@ function objectType(obj) {
 var checkEndpoint = {};
 
 checkEndpoint.http = function(endpointkey, cb) {
-	meetup[endpointkey](endpoints[endpointkey].test.params, function(err, ret) {
-		(function(errors) {
-			ret.problem = errors || ret.problem;
-			ret.code = errors && errors.code || ret.problem;
-			ret.details = errors && errors.message || ret.details;
+	if (endpoints[endpointkey].chunked) {
+		var chnk = meetup[endpointkey](endpoints[endpointkey].test.params)
+			.on('data', function(ret) {
+				chnk.abort();
+				assert.equal(objectType(ret), endpoints[endpointkey].test.return.type, endpointkey + ' not return an ' + endpoints[endpointkey].test.return.type);
+				switch (objectType(ret)) {
+					case 'object':
+						assert(endpoints[endpointkey].test.return.keys, endpointkey + ' hasn\'t defined keys on endpoints.json');
 
-		})(
-			ret && ret.errors && ret.errors.pop()
-		);
+						endpoints[endpointkey].test.return.keys.forEach(function(returnkey) {
+							assert(ret.hasOwnProperty(returnkey), endpointkey + ' not return the ' + returnkey + ' key');
+						});
 
-		assert(!ret.problem, ret.code + ' (' + ret.details + ')\n');
+						break;
+					default:
+				}
+			})
+			.on('end', function() {
+				console.log('%s\t\tPASS', endpointkey);
+				cb();
+			});
+	} else {
+		meetup[endpointkey](endpoints[endpointkey].test.params, function(err, ret) {
+			(function(errors) {
+				ret.problem = errors || ret.problem;
+				ret.code = errors && errors.code || ret.problem;
+				ret.details = errors && errors.message || ret.details;
 
-		assert(!err, err);
+			})(
+				ret && ret.errors && ret.errors.pop()
+			);
 
-		assert.equal(objectType(ret), endpoints[endpointkey].test.return.type, endpointkey + ' not return an ' + endpoints[endpointkey].test.return.type);
+			assert(!ret.problem, ret.code + ' (' + ret.details + ')\n');
 
-		switch (objectType(ret)) {
-			case 'object':
-				assert(endpoints[endpointkey].test.return.keys, endpointkey + ' hasn\'t defined keys on endpoints.json');
+			assert(!err, err);
 
-				endpoints[endpointkey].test.return.keys.forEach(function(returnkey) {
-					assert(ret.hasOwnProperty(returnkey), endpointkey + ' not return the ' + returnkey + ' key');
-				});
+			assert.equal(objectType(ret), endpoints[endpointkey].test.return.type, endpointkey + ' not return an ' + endpoints[endpointkey].test.return.type);
 
-				break;
-			default:
-		}
+			switch (objectType(ret)) {
+				case 'object':
+					assert(endpoints[endpointkey].test.return.keys, endpointkey + ' hasn\'t defined keys on endpoints.json');
 
-		console.log('%s\t\tPASS', endpointkey);
-		cb();
-	});
+					endpoints[endpointkey].test.return.keys.forEach(function(returnkey) {
+						assert(ret.hasOwnProperty(returnkey), endpointkey + ' not return the ' + returnkey + ' key');
+					});
+
+					break;
+				default:
+			}
+
+			console.log('%s\t\tPASS', endpointkey);
+			cb();
+		});
+	}
+
 };
 
 checkEndpoint.ws = function(endpointkey, cb) {
@@ -100,6 +126,8 @@ forAllAsync(meetup.commands
 	function(next, command) {
 		if (endpoints[command].resource.match(/^ws\:/)) {
 			checkEndpoint.ws(command, next);
+		} else if (endpoints[command].chunked) {
+			checkEndpoint.http(command, next);
 		} else {
 			setTimeout(function() {
 				checkEndpoint.http(command, next);
@@ -111,6 +139,6 @@ forAllAsync(meetup.commands
 
 process.on('exit', function(code) {
 	if (!code) {
-		console.log("\nDone!");
+		console.log('\nDone!');
 	}
 });
