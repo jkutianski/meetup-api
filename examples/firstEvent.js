05e64af220098ea829a6d02189439a8b226a66a8
Juan Pablo Kutianski <jkutianski@gmail.com>
2015-09-22 22:29:52 -0300

    Added example for getEvent()

---
+++ examples/firstEvent.js
@@ -0,0 +1,18 @@
+﻿var assert = require('assert');
+
+assert(process.env.MEETUP_KEY, 'MEETUP_KEY variable isn\'t set on enviroment (use \'set \"MEETUP_KEY=key\"\' on Windows)');
+
+var meetup = require('../lib/meetup')({
+    key: process.env.MEETUP_KEY
+});
+
+meetup.getEvent({
+    "urlname": "NodeJS-Argentina",
+    "id": "79797122"
+}, function(error, event) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(event);
+    }
+});
