280335b7da975be0f4615657ae463f7fe9069cd9
Juan Pablo Kutianski <jkutianski@gmail.com>
2015-10-01 01:41:04 -0300

    Added group samples

---
+++ examples/getgroup.js
@@ -0,0 +1,16 @@
+﻿var assert = require('assert');
+
+assert(process.env.MEETUP_KEY, 'MEETUP_KEY variable isn\'t set on enviroment (use \'set \"MEETUP_KEY=key\"\' on Windows)');
+
+var meetup = require('../lib/meetup')({
+    key: process.env.MEETUP_KEY
+});
+
+meetup.getGroup({
+    urlname: 'NodeJS-Argentina'
+}, function(err, resp) {
+    if (err) {
+        console.error('Found meetup error', err);
+    }
+    console.log(resp)
+});
