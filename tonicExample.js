4403c0bbd9056f27083b1182ecbf8c250bf073b4
Juan Pablo Kutianski <jkutianski@gmail.com>
2016-07-02 03:49:21 -0300

    Added endpoints getLocations, getSelfEvents, getSelfCalendar & getSelfGroups

--- tonicExample.js
+++ tonicExample.js
@@ -1,5 +1,11 @@
 var _meetup = require('meetup-api')();
 
+_meetup.getLocations({
+	"query": "córdoba"
+}, function (err, results) {
+    console.log(results);
+});
+
 _meetup.getCities({
 	lat: -34.603722,
 	lon: -58.381592,
