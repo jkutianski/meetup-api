326bd7732c05d1c4ed911b375d49043a098d2a4f
Juan Pablo Kutianski <jkutianski@gmail.com>
2017-03-07 07:18:38 -0300

    getRSVPs() example

---
+++ examples/getRSVPs.js
@@ -0,0 +1,17 @@
+﻿var assert = require('assert');
+
+assert(process.env.MEETUP_KEY, 'MEETUP_KEY variable isn\'t set on enviroment (use \'set \"MEETUP_KEY=key\"\' on Windows)');
+
+var meetup = require('../lib/meetup')({
+    key: process.env.MEETUP_KEY
+});
+
+meetup.getRSVPs({
+    event_id: 237407547,
+    urlname: 'banodejs'
+}, function(err, resp) {
+    if (err) {
+        console.error('Found meetup error', err);
+    }
+    console.log(JSON.stringify(resp, null, 2));
+});
