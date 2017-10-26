69252d3a787fee3fd9ee037bf93fd0bcc6baff78
Juan Pablo Kutianski <jkutianski@gmail.com>
2017-10-26 02:42:33 -0300

    comment proxy line

--- tonicExample.js
+++ tonicExample.js
@@ -1,5 +1,5 @@
 var _meetup = require('meetup-api')();
-var _proxy = require('proxy-agent'); // runkit workaround
+// var _proxy = require('proxy-agent'); // runkit workaround
 
 // _meetup.proxy = 'http://149.202.94.120:3128';
 
